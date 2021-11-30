e811df1429d318e4c4b1c05a7aa561e7f4f1293f
Pranay Yadav <pranayyadav08@gmail.com>
2021-11-30 22:07:39 +0530

    Preparing for deployment

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,6 +2,7 @@ import { SessionProvider } from "next-auth/react";
 import { AppProps } from "next/app";
 import { RecoilRoot } from "recoil";
 import "tailwindcss/tailwind.css";
+import Head from "next/head";
 
 import "../styles/global.css";
 
@@ -9,6 +10,9 @@ function MyApp({ Component, pageProps }: AppProps) {
   return (
     <SessionProvider session={pageProps.session}>
       <RecoilRoot>
+        <Head>
+          <title>Spotify Clone</title>
+        </Head>
         <Component {...pageProps} />
       </RecoilRoot>
     </SessionProvider>
--- pages/login.tsx
+++ pages/login.tsx
@@ -39,6 +39,7 @@ export async function getServerSideProps() {
     props: {
       providers,
     },
+    fallback: true,
   };
 }
 
